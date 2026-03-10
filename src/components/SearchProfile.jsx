eafe8011dd26dff6aa2ff6d71371906fa92cb9f4
Sirojiddin <gaybullayevs176@gmail.com>
2026-03-10 11:33:35 +0500

    Search results page is added by Samurai

--- src/components/SearchProfile.jsx
+++ src/components/SearchProfile.jsx
@@ -2,9 +2,12 @@ import { useEffect, useState } from "react";
 import { FaUser } from "react-icons/fa";
 import { useNavigate, useParams } from "react-router-dom"
 import "../styles/searchresultuser.css"
+import PostCard from "./PostCard";
+import getPostsByIds from "../utils/getPostsByIds";
 
 function SearchProfile() {
     const [data, setData] = useState(null);
+    const [posts, setPosts] = useState([]);
     const { usersID } = useParams();
     let navigate = useNavigate();
     useEffect(() => {
@@ -15,19 +18,38 @@ function SearchProfile() {
         }
         getUsers(usersID)
     }, [usersID])
-    return <div className="searchresultsuser">
-        <div className="userinfo">
-            <div className="userimg">
+
+    useEffect(() => {
+        (async function () {
+            if (data) {
+                const response = await getPostsByIds(data.posts);
+                setPosts(response);
+            };
+        })();
+    }, [data]);
+
+    if (!data) {
+        return <div>Loading...</div>
+    };
+    return <div className="profile-page">
+        <button onClick={() => navigate("/search")} className="back">Ortga</button>
+        <div className="UserInfo">
+            <div className="avatar">
                 {!data?.avatar ? <FaUser /> : <img src={data.avatar} />}
             </div>
-            <div className="usertext">
-                <h2>{data?.name ? data.name : "UserName"}</h2>
-                <p>{data?.email ? data.email : "UserEmail"}</p>
+            <div className="bio">
+                <div className="name">
+                    <h2>{data?.name ? data.name : "UserName"}</h2>
+                    <p>{data?.email ? data.email : "UserEmail"}</p>
+                    <p>{data?.bio ? data.bio : "not bio yet"}</p>
+                </div>
+                <div className="following">
+                    <button>Follow</button>
+                </div>
             </div>
-            <div className="follow-btn">
-                <button>Follow</button>
-                <button onClick={()=>navigate("/search")}>Ortga</button>
-            </div>
+        </div>
+        <div className="posts">
+            {data?.posts?.[0] ? posts.map(e => <PostCard post={e} userData={data} key={e.id} />) : "No posts yet"}
         </div>
     </div>
 }
