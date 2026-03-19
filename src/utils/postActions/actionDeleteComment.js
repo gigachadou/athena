9cdbfc89d6ab02b593b1b89714f90abe68973949
gigachadou <axmedovaasira@gmail.com>
2026-03-19 16:48:44 +0500

    Gigachadou: Now comment is deletable by it's owner and the post owner

--- src/utils/postActions/actionDeleteComment.js
+++ src/utils/postActions/actionDeleteComment.js
@@ -1,12 +1,12 @@
 /**
- * Komment qo'shish uchun async funksiya, try...catchda ishlatilsin
+ * Komment o'chirish uchun async funksiya, try...catchda ishlatilsin
  * @param {string} postId - post IDsi
  * @param {number} userId - login qilingan user IDsi
- * @param {string} text - komment teksti
+ * @param {string} commentId - comment Idsi
  */
-export default async function actionComment(postId, userId, text) {
-    //postga commentni joylash:
-    const commentId = userId + "-" + Date.now();
+export default async function actionDeleteComment(postId, userId, commentId) {
+    //postdan commentni olib tashlash:
+
     const res = await fetch(`http://localhost:3000/posts/${postId}`);
     if (!res.ok) throw new Error("Error at actionComment - GET1");
     const post = await res.json();
@@ -14,12 +14,12 @@ export default async function actionComment(postId, userId, text) {
     const res2 = await fetch(`http://localhost:3000/posts/${postId}`, {
         method: "PATCH",
         headers: { "Content-type": "application/json" },
-        body: JSON.stringify({ comments: [...post.comments, { id: commentId, user: userId, text: text }] })
+        body: JSON.stringify({ comments: post.comments.filter(c => c.id !== commentId) })
     });
 
     if (!res2.ok) throw new Error("Error at actionComment - PATCH1");
 
-    //userga commentni joylash ------------------------------------------>>>
+    //userdan commentni olib tashlash ------------------------------------------>>>
 
     const res3 = await fetch(`http://localhost:3000/users/${userId}`);
     if (!res3.ok) throw new Error("Error at actionComment - GET2");
@@ -28,7 +28,7 @@ export default async function actionComment(postId, userId, text) {
     const res4 = await fetch(`http://localhost:3000/users/${userId}`, {
         method: "PATCH",
         headers: { "Content-type": "application/json" },
-        body: JSON.stringify({ comments: [...user.comments, { id: commentId, post: postId, text: text }] })
+        body: JSON.stringify({ comments: user.comments.filter(c => c.id !== commentId) })
     });
     if (!res4.ok) throw new Error("Error at actionComment - PATCH2");
 };
